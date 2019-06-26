d1e0ccff3360a797cc9a9619afa3c159a4e8b0fa
wida <wida@foxmail.com>
2019-06-26 16:54:31 +0800

    first commit

---
+++ code/helloworld/helloworld.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8888", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/widaT/learning-go
+
+go 1.12
